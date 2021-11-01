ccea34f9a593bd83719b9a9db54458d7cb3aef24
volodymyr <v.ohlopkov100@gmail.com>
2021-11-01 22:16:09 +0200

    v 0.3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
-const btn = document.getElementById('btn');
+const btn = document.getElementById('btn'),
+      modal = document.getElementById('modal'),
+      closeMenu = document.querySelectorAll('.modal__links'),
+      social = document.querySelectorAll('.modal__social'),
+      toGo = document.querySelector('.modal__toGo'),
+      body = document.getElementById('body');
 
 btn.addEventListener('click', (e) => {
     e.target.classList.toggle('active');
-})
+    modal.classList.toggle('active');
+    body.classList.toggle('active');
+});
+
+closeMenu.forEach(el => el.addEventListener('click', ()=> {
+    btn.classList.remove('active');
+    modal.classList.remove('active');
+    body.classList.remove('active');
+  }));
+
+  social.forEach(el => el.addEventListener('click', ()=> {
+    btn.classList.remove('active');
+    modal.classList.remove('active');
+    body.classList.remove('active');
+  }));
+
+  toGo.addEventListener('click', (e) => {
+    if(e.target.tagName === 'A') {
+        btn.classList.remove('active');
+        modal.classList.remove('active');
+        body.classList.remove('active');
+    }
+});
