a1c6032ab64fdd7272760d74407a5e875e3aef8a
volodymyr <v.ohlopkov100@gmail.com>
2021-11-03 23:16:04 +0200

    v 0.5

--- js/app.js
+++ js/app.js
@@ -30,3 +30,26 @@ closeMenu.forEach(el => el.addEventListener('click', ()=> {
         body.classList.remove('active');
     }
 });
+
+var swiper = new Swiper(".brandsSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+   /*  el: ".swiper-pagination",
+    clickable: true, */
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
